6ba9ae362ce42915ff3e0ba6496439aed9c2d4c8
Suaad Sayyed <suaad.sayyed@mail.utoronto.ca>
2024-02-01 18:38:45 -0500

    Time: 70 ms (12.06%) | Memory: 11.9 MB (87.32%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        lr = 0
+        ud = 0
+
+        for i in moves:
+            if i == 'L':
+                lr +=1
+            if i == 'R':
+                lr -=1
+            if i == 'U':
+                ud +=1
+            if i == 'D':
+                ud -=1
+
+        if abs(lr) > 0 or abs(ud) > 0:
+            return False
+        else:
+            return True
