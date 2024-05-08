76164563c68b42393d4c46e85556eaa2ca4f9d7c
Suaad Sayyed <suaad.sayyed@mail.utoronto.ca>
2024-05-08 12:11:00 -0400

    Time: 1415 ms (39.27%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,47 +1,7 @@
 # Write your MySQL query statement below
-select s.user_id,
-case when c.requested > 0 then round(c.confirmed/c.requested,2)
-else 0.00
-end as confirmation_rate
-from signups s
-left join (
-    select user_id,
-    sum(case when action = 'confirmed' then 1 else 0 end) as confirmed,
-    count(*) as requested
-    from confirmations
-    group by user_id
-) c on s.user_id = c.user_id
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- SELECT 
---     s.user_id,
---     CASE 
---         WHEN c.total_requested > 0 THEN ROUND(c.confirmed / c.total_requested, 2)
---         ELSE 0.00
---     END AS confirmation_rate
--- FROM 
---     Signups s
--- LEFT JOIN (
---     SELECT 
---         user_id,
---         SUM(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END) AS confirmed,
---         COUNT(*) AS total_requested
---     FROM 
---         Confirmations
---     GROUP BY 
---         user_id
--- ) c ON s.user_id = c.user_id;
+SELECT s.user_id, 
+  ROUND(AVG(IF(c.action='confirmed',1,0)),2) as confirmation_rate 
+FROM Signups s
+LEFT JOIN Confirmations c using (user_id)
+GROUP BY s.user_id
