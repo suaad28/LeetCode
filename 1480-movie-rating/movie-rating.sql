743bed9f0c4c855c660fb3136c75447d509b3c89
Suaad Sayyed <suaad.sayyed@mail.utoronto.ca>
2024-06-26 18:24:04 -0400

    Time: 985 ms (96.87%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -3,8 +3,8 @@
 FROM users u
 JOIN movierating mr
 ON u.user_id = mr.user_id
-GROUP BY 1
-ORDER BY count(rating) desc, 1 asc
+GROUP BY name
+ORDER BY count(rating) desc, name asc  #for lexicographically
 Limit 1)
 
 UNION ALL
@@ -14,6 +14,6 @@ FROM movies m
 JOIN movierating mr
 ON m.movie_id = mr.movie_id
 WHERE month(created_at) = 2 and year(created_at) = 2020
-GROUP BY 1
-ORDER BY avg(rating) desc, 1 asc
+GROUP BY title
+ORDER BY avg(rating) desc, title asc #for lexicographically
 Limit 1)
